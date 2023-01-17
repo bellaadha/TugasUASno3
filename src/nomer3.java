6f34c49f0ed29a8758ff39c7d287519f0423e909
bella <iniintan880@gmail.com>
2023-01-17 12:34:08 +0700

    1D

--- src/nomer3.java
+++ src/nomer3.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class nomer3 {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class nomer3 {
+    public static void main(String[] args) {
+        public int intMessageID;
+        Private int intMessageName;
+        public int intContext;
+        public nomer3() {
+
+        }
+        int jum=4;
+        UAS1D myMessage = new UAS1D();
+        Scanner sc = new Scanner(System.in);
+        myMessage.setMessageID(sc.nextLine());
+        myMessage.setMessageName (sc.nextLine());
+        ArrayList<String>context = new ArrayList<>();
+        for(int index=0;index<jum;index++){
+            context.add(sc.nextLine());
+        }
+        MyMessage.setContext(context);
+
+        System.out.print("Kata yang dicari :");
+        String cari = sc.nextLine();
+        System.out.println(myMessage.FinWords(context,cari));
+
+
+    }
 }
